506be6b71750acf3cd88c10f2b5f3bb29d10f22c
masan4444 <masanori0213@gmail.com>
2022-01-10 06:04:30 +0900

    install command

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,14 +1,113 @@
-use structopt::StructOpt;
-
 use super::Command;
+use crate::curl;
+use crate::release;
+use crate::version::Version;
+use flate2::read::GzDecoder;
+use std::fs;
+use std::path::Path;
+use std::process;
+use std::str::FromStr;
+use structopt::StructOpt;
+use tar::Archive;
 
 #[derive(StructOpt, Debug)]
 pub struct Install {
-    version: String,
+    version: Option<String>,
 }
 
 impl Command for Install {
-    fn run(&self) {
-        println!("install {}", self.version)
+    fn run(&self) -> anyhow::Result<()> {
+        let home_dir = dirs::home_dir()
+            .expect("Can't get home directory")
+            .join(".phpup");
+        let versions_dir = home_dir.join("versions").join("php");
+
+        match &self.version {
+            Some(version) => {
+                let version = Version::from_str(version)?;
+                let release = release::fetch_latest(version)?;
+                let install_version = release.version.unwrap();
+                println!("Installing {}...", install_version);
+                let url = release.source_url();
+
+                let install_dir = versions_dir.join(install_version.to_string());
+                fs::create_dir_all(&install_dir).unwrap();
+                let download_dir = tempfile::Builder::new()
+                    .prefix(".download-")
+                    .tempdir_in(&install_dir)?;
+                println!("Downloading {}...", url);
+                Self::download_and_unpack(&url, &download_dir);
+
+                let source_dir = fs::read_dir(&download_dir.path())
+                    .unwrap()
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .path();
+                println!("Buiding...");
+                Self::build(source_dir, install_dir).unwrap();
+            }
+            None => {}
+        };
+        Ok(())
+    }
+}
+
+impl Install {
+    // TODO: checksum
+    fn download_and_unpack(url: &str, path: impl AsRef<Path>) {
+        let response = curl::get_as_reader(url);
+        let gz_decoder = GzDecoder::new(response);
+        let mut tar_archive = Archive::new(gz_decoder);
+        tar_archive.unpack(path).unwrap();
+    }
+
+    fn build(src_dir: impl AsRef<Path>, dist_dir: impl AsRef<Path>) -> Result<(), ()> {
+        let mut command = process::Command::new("sh");
+
+        println!("./configure");
+        command
+            .arg("configure")
+            .arg(format!("--prefix={}", dist_dir.as_ref().to_str().unwrap()));
+        // .args(configure_opts);
+
+        let configure = command.current_dir(&src_dir).output().unwrap();
+        if !configure.status.success() {
+            println!(
+                "configure failed: {}",
+                String::from_utf8_lossy(&configure.stderr).to_string()
+            );
+            return Err(());
+        };
+
+        println!("./make");
+        let make = process::Command::new("make")
+            .arg("-j")
+            .arg(num_cpus::get().to_string())
+            .current_dir(&src_dir)
+            .output()
+            .unwrap();
+        if !make.status.success() {
+            println!(
+                "make failed: {}",
+                String::from_utf8_lossy(&make.stderr).to_string()
+            );
+            return Err(());
+        };
+
+        println!("./make install");
+        let make_install = process::Command::new("make")
+            .arg("install")
+            .current_dir(&src_dir)
+            .output()
+            .unwrap();
+        if !make_install.status.success() {
+            println!(
+                "make install: {}",
+                String::from_utf8_lossy(&make_install.stderr).to_string()
+            );
+            return Err(());
+        };
+        Ok(())
     }
 }
