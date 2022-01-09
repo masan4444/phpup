fd9b64f5f75c92168b1ae4a5807c06f19dbe324f
masan4444 <masanori0213@gmail.com>
2022-01-10 06:03:33 +0900

    sub commands module

--- src/commands.rs
+++ src/commands.rs
@@ -1,13 +1,17 @@
+use anyhow::Result;
+
 pub trait Command {
-    fn run(&self) {}
+    fn run(&self) -> Result<()>;
 }
 
+mod init;
 mod install;
-mod list;
+mod list_local;
 mod list_remote;
 mod r#use;
 
+pub use init::Init;
 pub use install::Install;
-pub use list::List;
+pub use list_local::ListLocal;
 pub use list_remote::ListRemote;
 pub use r#use::Use;
