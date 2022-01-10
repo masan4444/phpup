bc8ce96283f7ce7a8b2d9d927dc2c413c8676fa7
masan4444 <masanori0213@gmail.com>
2022-01-11 05:19:50 +0900

    remove support list feature

--- src/commands/list_local.rs
+++ src/commands/list_local.rs
@@ -1,7 +1,5 @@
 use super::{Command, Config};
-use crate::release::{Release, Support};
 use crate::version::Version;
-use std::collections::BTreeMap;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -12,17 +10,16 @@ pub struct ListLocal {
 impl Command for ListLocal {
     fn run(&self, config: &Config) -> anyhow::Result<()> {
         let local_versions = &config.local_versions;
-        let empty = vec![];
-        let printer = Printer::new(&empty, config.current_version);
+        let printer = Printer::new(&[], config.current_version);
 
         match &self.version {
             Some(version) => local_versions
                 .iter()
                 .filter(|local_version| version.contains(local_version))
-                .for_each(|&local_version| printer.print_version(local_version, None)),
+                .for_each(|&local_version| printer.print_version(local_version)),
             None => local_versions
                 .iter()
-                .for_each(|&local_version| printer.print_version(local_version, None)),
+                .for_each(|&local_version| printer.print_version(local_version)),
         };
 
         Ok(())
@@ -31,24 +28,22 @@ impl Command for ListLocal {
 
 // TODO: refactor
 pub struct Printer<'a> {
-    local_versions: &'a Vec<Version>,
-    supports: BTreeMap<Version, Support>,
+    local_versions: &'a [Version],
     current_version: Option<Version>,
 }
 
 impl<'a> Printer<'a> {
-    pub fn new(local_versions: &'a Vec<Version>, current_version: Option<Version>) -> Self {
+    pub fn new(local_versions: &'a [Version], current_version: Option<Version>) -> Self {
         Self {
             local_versions,
-            supports: BTreeMap::new(),
             current_version,
         }
     }
-    pub fn print_version(&self, version: Version, support: Option<Support>) {
+    pub fn print_version(&self, version: Version) {
         let installed = self.local_versions.contains(&version);
         let used = self.current_version == Some(version);
         println!(
-            "{:<3}{:<7}   {}",
+            "{:<3}{:<7}",
             if used {
                 "->"
             } else {
@@ -59,23 +54,11 @@ impl<'a> Printer<'a> {
                 }
             },
             version.to_string(),
-            support.map_or("".to_owned(), |s| format!("({})", s.to_string())),
         );
     }
-    fn print_release(&mut self, version: Version, release: &Release) {
-        let minor_version =
-            Version::from_numbers(version.major_version(), version.minor_version(), None);
-        let support = if version.patch_version() == Some(0) {
-            let support = release.calculate_support();
-            Some(*self.supports.entry(minor_version).or_insert(support))
-        } else {
-            self.supports.get(&minor_version).cloned()
-        };
-        self.print_version(version, support)
-    }
-    pub fn print_releases(&mut self, releaes: &BTreeMap<Version, Release>) {
-        for (&version, release) in releaes {
-            self.print_release(version, release);
+    pub fn print_versions(&self, releaes: impl Iterator<Item = &'a Version>) {
+        for version in releaes {
+            self.print_version(*version);
         }
     }
 }
--- src/commands/list_remote.rs
+++ src/commands/list_remote.rs
@@ -7,27 +7,27 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 pub struct ListRemote {
     version: Option<Version>,
+    #[structopt(long)]
+    old: bool,
 }
 
 impl Command for ListRemote {
     fn run(&self, config: &Config) -> anyhow::Result<()> {
         let local_versions = &config.local_versions;
-        let mut printer = Printer::new(local_versions, config.current_version);
+        let printer = Printer::new(local_versions, config.current_version);
 
         match &self.version {
             Some(version) => {
-                if version.patch_version().is_some() {
-                    let oldest_patch_release = release::fetch_oldest_patch(*version)?;
-                    let support = oldest_patch_release.calculate_support();
-                    printer.print_version(*version, Some(support));
-                } else {
-                    printer.print_releases(&release::fetch_all(*version)?);
-                }
+                printer.print_versions(release::fetch_all(*version)?.keys());
             }
             None => {
-                // Self::print_releases(&release::fetch_all_releases(Version::from_major(5))?);
-                printer.print_releases(&release::fetch_all(Version::from_major(7))?);
-                printer.print_releases(&release::fetch_all(Version::from_major(8))?);
+                if self.old {
+                    printer.print_versions(release::fetch_all(Version::from_major(3))?.keys());
+                    printer.print_versions(release::fetch_all(Version::from_major(4))?.keys());
+                    printer.print_versions(release::fetch_all(Version::from_major(5))?.keys());
+                }
+                printer.print_versions(release::fetch_all(Version::from_major(7))?.keys());
+                printer.print_versions(release::fetch_all(Version::from_major(8))?.keys());
             }
         };
         Ok(())
